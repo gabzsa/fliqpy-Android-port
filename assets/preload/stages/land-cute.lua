0141e62ac76a25267fabee985df0f97449365a6d
gabzsa <109770528+gabzsa@users.noreply.github.com>
2022-11-13 09:42:17 -0300

    Add files via upload

---
+++ assets/preload/stages/land-cute.lua
@@ -0,0 +1,15 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bgWalls', 'lands/land-cute/bgWalls', -790, -360);
+	setLuaSpriteScrollFactor('bgWalls', 0.9, 0.9);
+	scaleObject('bgWalls', 0.8, 0.8);
+	
+	makeLuaSprite('fgSnow', 'lands/land-cute/fgSnow', -640, 670);
+	setLuaSpriteScrollFactor('fgSnow', 0.9, 0.9);
+	scaleObject('fgSnow', 1.0, 1.1);
+
+	addLuaSprite('bgWalls', false);
+	addLuaSprite('fgSnow', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
---
+++ assets/preload/stages/land-destroyed.lua
@@ -0,0 +1,14 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('stageback', 'lands/land-destroyed/stageback', -500, -300);
+	setLuaSpriteScrollFactor('stageback', 0.9, 0.9);
+	
+	makeLuaSprite('stagefront', 'lands/land-destroyed/stagefront', -650, 600);
+	setLuaSpriteScrollFactor('stagefront', 0.9, 0.9);
+	scaleObject('stagefront', 1.1, 1.1);
+
+	addLuaSprite('stageback', false);
+	addLuaSprite('stagefront', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
